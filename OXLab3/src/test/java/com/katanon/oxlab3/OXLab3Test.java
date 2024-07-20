57b6271c1bda594e8c590a5ce6260b4df18dbfa0
KTN-katanon <66160196@go.buu.ac.th>
2024-07-20 19:22:28 +0700

    Update OXLab3Test.java
    Finish checkWinner_Col3_true_byX

--- OXLab3/src/test/java/com/katanon/oxlab3/OXLab3Test.java
+++ OXLab3/src/test/java/com/katanon/oxlab3/OXLab3Test.java
@@ -88,4 +88,12 @@ public class OXLab3Test {
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
    }
+   
+   @Test
+   public void checkWinner_Col3_true_byX(){
+        char[][] board = {{'-','-', 'X'}, {'-', '-', 'X'},{'-','-', 'X'}};
+        char currentPlayer = 'X';
+        boolean result = OXLab3.checkWinner(board, currentPlayer);
+        assertTrue(result);
+   }
 }
