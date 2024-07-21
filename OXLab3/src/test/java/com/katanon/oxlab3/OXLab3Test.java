5fc1c1daa5e9952dc944fd3d4b080cc59cc18203
KTN-katanon <66160196@go.buu.ac.th>
2024-07-21 10:22:33 +0700

    Update OXLab3Test.java
    format code

--- OXLab3/src/test/java/com/katanon/oxlab3/OXLab3Test.java
+++ OXLab3/src/test/java/com/katanon/oxlab3/OXLab3Test.java
@@ -16,168 +16,171 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author AVI003
  */
 public class OXLab3Test {
-    
+
     public OXLab3Test() {
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
 
-   @Test
-   public void swapTurn_X_O(){
-       char currentPlayer = 'X';
-       char result = OXLab3.swapTurn(currentPlayer);
-       assertEquals('O', result);
-   }
-   
-   @Test
-   public void swapTurn_O_X(){
-       char currentPlayer = 'O';
-       char result = OXLab3.swapTurn(currentPlayer);
-       assertEquals('X', result);
-   }
-   
-   @Test
-   public void checkWinner_Row1_true_byX(){
+    @Test
+    public void swapTurn_X_O() {
+        char currentPlayer = 'X';
+        char result = OXLab3.swapTurn(currentPlayer);
+        assertEquals('O', result);
+    }
+
+    @Test
+    public void swapTurn_O_X() {
+        char currentPlayer = 'O';
+        char result = OXLab3.swapTurn(currentPlayer);
+        assertEquals('X', result);
+    }
+
+    @Test
+    public void checkWinner_Row1_true_byX() {
         char[][] board = {{'X', 'X', 'X'}, {'-', '-', '-'}, {'-', '-', '-'}};
         char currentPlayer = 'X';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   
+    }
+
     @Test
-   public void checkWinner_Row2_true_byX(){
-        char[][] board = {{'-', '-', '-'},{'X', 'X', 'X'}, {'-', '-', '-'}};
+    public void checkWinner_Row2_true_byX() {
+        char[][] board = {{'-', '-', '-'}, {'X', 'X', 'X'}, {'-', '-', '-'}};
         char currentPlayer = 'X';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   
-   @Test
-   public void checkWinner_Row3_true_byX(){
-        char[][] board = {{'-', '-', '-'}, {'-', '-', '-'},{'X', 'X', 'X'}};
+    }
+
+    @Test
+    public void checkWinner_Row3_true_byX() {
+        char[][] board = {{'-', '-', '-'}, {'-', '-', '-'}, {'X', 'X', 'X'}};
         char currentPlayer = 'X';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   @Test
-   public void checkWinner_Col1_true_byX(){
-        char[][] board = {{'X', '-', '-'}, {'X', '-', '-'},{'X', '-', '-'}};
+    }
+
+    @Test
+    public void checkWinner_Col1_true_byX() {
+        char[][] board = {{'X', '-', '-'}, {'X', '-', '-'}, {'X', '-', '-'}};
         char currentPlayer = 'X';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   
-   @Test
-   public void checkWinner_Col2_true_byX(){
-        char[][] board = {{'-','X', '-'}, {'-', 'X', '-'},{'-','X', '-'}};
+    }
+
+    @Test
+    public void checkWinner_Col2_true_byX() {
+        char[][] board = {{'-', 'X', '-'}, {'-', 'X', '-'}, {'-', 'X', '-'}};
         char currentPlayer = 'X';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   
-   @Test
-   public void checkWinner_Col3_true_byX(){
-        char[][] board = {{'-','-', 'X'}, {'-', '-', 'X'},{'-','-', 'X'}};
+    }
+
+    @Test
+    public void checkWinner_Col3_true_byX() {
+        char[][] board = {{'-', '-', 'X'}, {'-', '-', 'X'}, {'-', '-', 'X'}};
         char currentPlayer = 'X';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   
-   @Test
-   public void checkWinner_Diag1_true_byX(){
-        char[][] board = {{'X','-', '-'}, {'-', 'X', '-'},{'-','-','X'}};
+    }
+
+    @Test
+    public void checkWinner_Diag1_true_byX() {
+        char[][] board = {{'X', '-', '-'}, {'-', 'X', '-'}, {'-', '-', 'X'}};
         char currentPlayer = 'X';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   
-   @Test
-   public void checkWinner_Diag2_true_byX(){
-        char[][] board = {{'-','-', 'X'}, {'-', 'X', '-'},{'X','-', '-'}};
+    }
+
+    @Test
+    public void checkWinner_Diag2_true_byX() {
+        char[][] board = {{'-', '-', 'X'}, {'-', 'X', '-'}, {'X', '-', '-'}};
         char currentPlayer = 'X';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   
-   @Test
-   public void checkWinner_Diag1_true_byO(){
-        char[][] board = {{'O','-', '-'}, {'-', 'O', '-'},{'-','-','O'}};
+    }
+
+    @Test
+    public void checkWinner_Diag1_true_byO() {
+        char[][] board = {{'O', '-', '-'}, {'-', 'O', '-'}, {'-', '-', 'O'}};
         char currentPlayer = 'O';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   
-   @Test
-   public void checkWinner_Diag2_true_byO(){
-        char[][] board = {{'-','-', 'O'}, {'-', 'O', '-'},{'O','-', '-'}};
+    }
+
+    @Test
+    public void checkWinner_Diag2_true_byO() {
+        char[][] board = {{'-', '-', 'O'}, {'-', 'O', '-'}, {'O', '-', '-'}};
         char currentPlayer = 'O';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   
-   @Test
-   public void checkWinner_Row1_false_byX(){
+    }
+
+    @Test
+    public void checkWinner_Row1_false_byX() {
         char[][] board = {{'X', 'X', '-'}, {'-', '-', '-'}, {'-', '-', '-'}};
         char currentPlayer = 'X';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertFalse(result);
-   }
-   
-   @Test
-   public void checkWinner_Row1_true_byO(){
+    }
+
+    @Test
+    public void checkWinner_Row1_true_byO() {
         char[][] board = {{'O', 'O', 'O'}, {'-', '-', '-'}, {'-', '-', '-'}};
         char currentPlayer = 'O';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   
-   @Test
-   public void checkWinner_Col1_true_byO(){
+    }
+
+    @Test
+    public void checkWinner_Col1_true_byO() {
         char[][] board = {{'O', '-', '-'}, {'O', '-', '-'}, {'O', '-', '-'}};
         char currentPlayer = 'O';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertTrue(result);
-   }
-   @Test
-   public void checkWinner_Col1_false_byO(){
+    }
+
+    @Test
+    public void checkWinner_Col1_false_byO() {
         char[][] board = {{'O', '-', '-'}, {'O', '-', '-'}, {'-', '-', '-'}};
         char currentPlayer = 'O';
         boolean result = OXLab3.checkWinner(board, currentPlayer);
         assertFalse(result);
-   }
-   @Test
-   public void checkDraw_boardFull_true(){
-        char[][] board = {{'O', 'X', 'O'}, {'O', 'X', 'O'}, {'X', 'O', 'X'}};  
+    }
+
+    @Test
+    public void checkDraw_boardFull_true() {
+        char[][] board = {{'O', 'X', 'O'}, {'O', 'X', 'O'}, {'X', 'O', 'X'}};
         boolean result = OXLab3.checkDraw(board);
         assertTrue(result);
-   }
-   
-   @Test
-   public void playAgain_yes_true(){
-       String play = "yes";
-       boolean result = OXLab3.playAgain(play);
-       assertTrue(result);
-   }
-   
-   @Test
-   public void playAgain_no_true(){
-       String play = "no";
-       boolean result = OXLab3.playAgain(play);
-       assertFalse(result);
-   }
+    }
+
+    @Test
+    public void playAgain_yes_true() {
+        String play = "yes";
+        boolean result = OXLab3.playAgain(play);
+        assertTrue(result);
+    }
+
+    @Test
+    public void playAgain_no_true() {
+        String play = "no";
+        boolean result = OXLab3.playAgain(play);
+        assertFalse(result);
+    }
 
 }
